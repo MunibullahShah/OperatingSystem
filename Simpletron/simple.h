e5a2a1073525f5b88a7263397d39e32bc228fb07
Munibullah Shah <49975985+MunibullahShah@users.noreply.github.com>
2021-03-06 11:26:33 +0500

    Add files via upload

---
+++ Simpletron/simple.h
@@ -0,0 +1,12 @@
+#define READ 10
+#define WRITE 11
+#define LOAD 20
+#define STORE 21
+#define ADD 30
+#define SUBTRACT 31
+#define DIVIDE 32
+#define MULTIPLY 33
+#define BRANCH 40
+#define BRANCHNEG 41
+#define BRANCHZERO 42
+#define HALT 43
